1d62109f67c09b7f684f82670d61bc3bcf65c0b4
Davion <davion2017@foxmail.com>
2022-01-19 15:13:23 +0800

    add springboot application

---
+++ src/main/java/cn/davion/githubaction/GithubAction.java
@@ -0,0 +1,17 @@
+package cn.davion.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Davion
+ * @date 2022/01/19
+ */
+@SpringBootApplication
+public class GithubAction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubAction.class, args);
+    }
+
+}
